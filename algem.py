b0205660dadf241c3e233ca11a0834b873024ee0
jolyrain <jolyrain@mail.ru>
2020-06-03 21:17:01 +0300

    Окончательная (финальная (уже точно)) доработка!

--- algem.py
+++ algem.py
@@ -5,20 +5,26 @@ from pylab import *
 
 # Класс, описывающий астроиду
 class Astroid:
-    EQUATION = r'$x^{\frac{2}{3}}+y^{\frac{2}{3}}=R^{\frac{2}{3}}$'
     ANGLE = np.linspace(0, 2 * pi, 1000)
-    radius = 0
+    is_evolute = False
+    parameter_a = 0
+    parameter_b = 0
     x = 0
     y = 0
 
-    def __init__(self, radius):
-        self.radius = radius
+    def __init__(self, parameter_a, parameter_b=None):
+        self.parameter_a = parameter_a
+        if parameter_b is None:
+            self.parameter_b = parameter_a
+        else:
+            self.parameter_b = parameter_b
+            self.is_evolute = True
         self.__function()
 
     # Параметрическое уравнение астроиды
     def __function(self):
-        self.x = self.radius * pow(cos(self.ANGLE), 3)
-        self.y = self.radius * pow(sin(self.ANGLE), 3)
+        self.x = self.parameter_a * pow(cos(self.ANGLE), 3)
+        self.y = self.parameter_b * pow(sin(self.ANGLE), 3)
 
 
 # "Красивое" поле для графика
@@ -41,17 +47,23 @@ def coordinate_axes():
 
 # Класс, рисующий график астроиды
 class Graphic:
+    title = None
     astroid = None
     BORDER = 10
 
     def __init__(self, astroid):
         self.astroid = astroid
+        if astroid.is_evolute:
+            self.title = 'Астроида\n' + \
+                         'при a = ' + str(self.astroid.parameter_a) + \
+                         ' и b = ' + str(self.astroid.parameter_b)
+        else:
+            self.title = 'Астроида\n' + 'при R = ' + str(self.astroid.parameter_a)
 
     def show(self):
         fig = plt.figure(figsize=(self.BORDER, self.BORDER))
         ax = fig.add_subplot()
-        ax.set_title('Астроида\n' + self.astroid.EQUATION +
-                     ', при R = ' + str(self.astroid.radius), pad=20, fontsize=30)
+        ax.set_title(self.title, pad=20, fontsize=30)
         ax.set(xlim=[-self.BORDER, self.BORDER],
                ylim=[-self.BORDER, self.BORDER])
         coordinate_axes()
@@ -59,7 +71,6 @@ class Graphic:
         plt.show()
 
 
-radius_input = int(input('radius = '))
-new_astroid = Astroid(radius_input)
+new_astroid = Astroid(10, 2)
 graphic = Graphic(new_astroid)
 graphic.show()
